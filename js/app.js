5e8492dff0b65f1c6840030536f4af2c114683e5
ateev <ateev@live.com>
2012-12-26 01:27:37 +0530

    basic structure made

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-var hashtag;
-var twitterapi = "http://search.twitter.com/search.json?q=Pink&rpp=1000" ;
+$(document).ready(function(){
+$('submit').click(function(hashtag){
 
-$.getJSON(twitterapi + 'q=' + hashtag + '&' + 'rpp=1500',  function(data){
+   
+	var twitterapi = "http://search.twitter.com/search.json?";
+	var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500'
+    $.getJSON(url,function(results){
+	for(i=0;i<100;i++)
+	{
+			$("#tweet").html("<p> Name: " + results[0].from_user + " -   " + results[0].text + "</p>");
+	}
+	
+    });	
+});
