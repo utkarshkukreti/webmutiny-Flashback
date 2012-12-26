32e892730f405786ff14a9b938d206949e08f9e4
ateev <ateev@live.com>
2012-12-26 15:21:34 +0530

    looping fixed

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@ $('submit').click(function(hashtag){
 	
 
     var array;
+    var j=0;
 	var twitterapi = "http://search.twitter.com/search.json?";
-	var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500'
+	var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500';
+	length = results.length;
     $.getJSON(url,function(results){
-	for(i=0;i<100;i++)
+	for(i=0;i<length;i++)
 	{
-			$("#tweet").html("<p> Name: " + results[0].from_user + " -   " + results[0].text + "</p>");
-			array[i] = [results[0].from_user,results[0].text];
+			$("#tweet").html("<p> Name: " + results[i].from_user + " -   " + results[i].text + "</p>");
+			array[j] = [results[i].from_user,results[i].text];
+			j++;
 			
 	}
 	
