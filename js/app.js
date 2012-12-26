83397c634df2bd240b254f3cad3af0251e0f2ce8
ateev <ateev@live.com>
2012-12-26 14:35:45 +0530

    some changes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 $('submit').click(function(hashtag){
+	
 
-   
+    var array;
 	var twitterapi = "http://search.twitter.com/search.json?";
 	var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500'
     $.getJSON(url,function(results){
 	for(i=0;i<100;i++)
 	{
 			$("#tweet").html("<p> Name: " + results[0].from_user + " -   " + results[0].text + "</p>");
+			array[i] = [results[0].from_user,results[0].text];
+			
 	}
 	
     });	
