0a359da3242f61f952992e71cdddbf4e3f13289a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-27 14:41:43 +0530

    Fix. Everything.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,23 @@
-$(document).ready(function()
-{
-  $('submit').click(function(hashtag)
-    {
-      var array;
-      var j=0;
-      var twitterapi = "http://search.twitter.com/search.json?";
-      var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500';
-      tweetspresent = results.length;
-      jQuery.ajax(
-    	{
-          type: "GET",
-          url:  twitterapi ,
-          data: {"q": hashtag,"rpp":1000},
-          dataType: 'jsonp',
-        });
-    }).done(function(results)
-        {
-           for(i=0;i<tweetspresent;i++)
-             {
-               $("#tweet").html("<p> Name: " + results[i].from_user + " -   " + results[i].text + "</p>");
-               array[j] = [results[i].from_user,results[i].text];
-               j++;
-             }
-        });
+$(document).ready(function () {
+  $('#search').submit(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var hashtag = $(this).find('input[name="hashtag"]').val();
+    var twitterapi = "http://search.twitter.com/search.json?";
+    jQuery.ajax({
+      type: "GET",
+      url: twitterapi,
+      data: {
+        "q": hashtag,
+        "rpp": 1000
+      },
+      dataType: 'jsonp'
+    }).done(function (response) {
+      var results = response.results;
+      for (var i = 0; i < results.length; i++) {
+        $("#tweet").append("<p> Name: " + results[i].from_user + " -   " +
+                           results[i].text + "</p>");
+      }
+    });
+  });
 });
