8a6994b18b6da87c11024253902bbb41b1929f25
ateev <ateev@live.com>
2012-12-27 14:15:43 +0530

    indented

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-$(document).ready(function(){
-$('submit').click(function(hashtag){
-	
-
-    var array;
-    var j=0;
-	var twitterapi = "http://search.twitter.com/search.json?";
-	var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500';
-	length = results.length;
-    $.getJSON(url,function(results){
-	for(i=0;i<length;i++)
-	{
-			$("#tweet").html("<p> Name: " + results[i].from_user + " -   " + results[i].text + "</p>");
-			array[j] = [results[i].from_user,results[i].text];
-			j++;
-			
-	}
-	
-    });	
+$(document).ready(function()
+{
+  $('submit').click(function(hashtag)
+    {
+      var array;
+      var j=0;
+      var twitterapi = "http://search.twitter.com/search.json?";
+      var link = twitterapi + 'q=' + hashtag + '&' + 'rpp=1500';
+      tweetspresent = results.length;
+      jQuery.ajax(
+    	{
+          type: "GET",
+          url:  twitterapi ,
+          data: {"q": hashtag,"rpp":1000},
+          dataType: 'jsonp',
+        });
+    }).done(function(results)
+        {
+           for(i=0;i<tweetspresent;i++)
+             {
+               $("#tweet").html("<p> Name: " + results[i].from_user + " -   " + results[i].text + "</p>");
+               array[j] = [results[i].from_user,results[i].text];
+               j++;
+             }
+        });
 });
